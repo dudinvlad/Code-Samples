3dd0c4408d9ee26720749f3ccf52af9e67c07a6c
dudinvlad <dudin1.vlad@gmail.com>
2017-09-25 10:53:37 +0300

    Update IZHomeViewController.m

--- IZHomeViewController.m
+++ IZHomeViewController.m
@@ -1,10 +1,4 @@
-//
-//  IZHomeViewController.m
-//  MediMee
-//
-//  Created by Vladislav Dudin on 11/22/16.
-//  Copyright © 2016 Vladislav Dudin. All rights reserved.
-//
+
 
 #import "IZHomeViewController.h"
 #import "IZBaseViewController+Protected.h"
