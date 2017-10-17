8e0b5165f7f607346ea2d37a60daf0577108fbbe
dudinvlad <dudin1.vlad@gmail.com>
2017-10-17 16:09:31 +0300

    Add files via upload

---
+++ IZMailManager.m
@@ -0,0 +1,67 @@
+//
+//  IZMailManager.m
+//  MediMee
+//
+//  Created by Vladislav Dudin on 1/30/17.
+//  Copyright © 2017 Vladislav Dudin. All rights reserved.
+//
+
+#import "IZMailManager.h"
+
+@interface IZMailManager () <MFMailComposeViewControllerDelegate>
+
+@property (copy, nonatomic) void (^completionBlock)(MFMailComposeViewController *,MFMailComposeResult, NSError *);
+
+@end
+
+@implementation IZMailManager
+
+- (void)dealloc {
+    NSLog(@"Im free");
+}
+
+#pragma mark - Public -
+
+-(void)sendEmailWithSubject:(NSString *)subject messageBody:(NSString *)body recipients:(NSArray<NSString *> *)recipients presenter:(UIViewController *)viewController completionBlock:(void(^)(MFMailComposeViewController *,MFMailComposeResult, NSError *))block {
+    if (![MFMailComposeViewController canSendMail]) {
+        NSLog(@"Mail services are not available.");
+        return;
+    }
+    self.completionBlock = block;
+    MFMailComposeViewController *mc = [[MFMailComposeViewController alloc] init];
+    mc.mailComposeDelegate = self;
+    [mc setSubject:subject];
+    [mc setMessageBody:body isHTML:NO];
+    [mc setToRecipients:recipients];
+    
+    // Present mail view controller on screen
+    [viewController presentViewController:mc animated:YES completion:NULL];
+}
+
+#pragma mark - <MFMailComposeViewControllerDelegate> -
+
+- (void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(nullable NSError *)error {
+    switch (result)
+    {
+        case MFMailComposeResultCancelled:
+            NSLog(@"Mail cancelled");
+            break;
+        case MFMailComposeResultSaved:
+            NSLog(@"Mail saved");
+            break;
+        case MFMailComposeResultSent:
+            NSLog(@"Mail sent");
+            break;
+        case MFMailComposeResultFailed:
+            NSLog(@"Mail sent failure: %@", [error localizedDescription]);
+            break;
+        default:
+            break;
+    }
+    
+    if (self.completionBlock) {
+        self.completionBlock(controller, result, error);
+    }
+ }
+
+@end
